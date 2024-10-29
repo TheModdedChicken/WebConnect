7f267ba3d30ca67ca7c12f80533c80457a395f1c
TheModdedChicken <50579513+TheModdedChicken@users.noreply.github.com>
2024-10-29 23:34:31 +0800

    File cleanup

--- web/.svelte-kit/output/client/_app/immutable/entry/start.DQxTrPsp.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.DaIJGEIz.js";export{t as start};
--- web/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.C9sDej0k.js","_app/immutable/chunks/disclose-version.B6Sji2Jz.js","_app/immutable/chunks/runtime.DgFCWXxz.js","_app/immutable/chunks/store.BVe0zDLp.js","_app/immutable/chunks/entry.DaIJGEIz.js"];
-export const stylesheets = [];
-export const fonts = [];
